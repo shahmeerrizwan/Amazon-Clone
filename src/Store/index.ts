8784a9da89884ee5c5e6d940346458b8ae0491f4
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-25 17:00:36 +0500

    Amazon Clone

---
+++ src/Store/index.ts
@@ -0,0 +1,40 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+import cartReducer from './CartSlice';
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+
+const rootReducer = {
+    cart: persistReducer(persistConfig, cartReducer),
+};
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+const persister = persistStore(store);
+
+export { store, persister };
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
